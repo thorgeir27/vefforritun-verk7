7353f56a17c2d1cc0532614bda12b69dbb6a7c16
Þorgeir <thorgeir93@gmail.com>
2018-10-30 17:54:53 +0000

    kláraði verkefnið

--- script.js
+++ script.js
@@ -13,7 +13,22 @@ const GAMES_TO_PLAY = 10;
  * Eftir leik er notanda boðið að spila annan leik, ef ekki hættir forrit.
  */
 function start() {
-  villa;
+  //villa;
+  alert('Markmiðið er að svara eins mörgum af 10 dæmum rétt eins hratt og mögulegt er.')
+  play();
+
+  let playAnother = true;
+
+  while (playAnother) {
+    playAnother = confirm('Spila annan leik?');
+
+    if (playAnother) {
+      play();
+    } else {
+      playAnother = false;
+      break;
+    }
+  }
 }
 
 /**
@@ -28,6 +43,29 @@ function start() {
  *
  */
 function play() {
+  let timeStart = new Date;
+  let correctAnswers = 0;
+
+  for (let i = 1 ; i <= GAMES_TO_PLAY ; i = i + 1) {
+    let answer = ask();
+    if (answer === null) {
+      alert('Hætt í leik.')
+      break;
+    } else {
+      correctAnswers = correctAnswers + answer;
+
+    }
+
+  }
+
+  if (answer !== null) {
+    let timeEnd = new Date;
+    let totalTime = (timeEnd - timeStart) / 1000;
+    averageTime = correctAnswers / totalTime;
+  
+    alert(`Þú svaraðir ${correctAnswers} af ${GAMES_TO_PLAY} dæmum rétt á ${totalTime.toFixed(2)} sekúndum\nMeðalrétt svör á sekúndu eru ${averageTime.toFixed(2)}`);
+  }
+
 }
 
 /**
@@ -45,6 +83,38 @@ function play() {
  * Sniðugt væri að færa það að búa til spurningu í nýtt fall sem ask() kallar í.
  */
 function ask() {
+  let a;
+  let b;
+  let c;
+  
+  let symbol = symbolDecider(randomNumber(1,4));
+
+  if (symbol === '/') {
+    b = randomNumber(2,10);
+    a = b*randomNumber(2,10);
+    c = a/b;
+  } else if (symbol ==='*') {
+    a = randomNumber(1,10);
+    b = randomNumber(1,10);
+    c = a*b;
+  } else if (symbol === '-') {
+    a = randomNumber(1,100);
+    b = randomNumber(1,100);
+    c = a-b;
+  } else {
+    a = randomNumber(1,100);
+    b = randomNumber(1,100);
+    c = a+b;
+  }
+  let userAnswer = prompt(`Hvað er ${a} ${symbol} ${b}?`);
+
+  if (userAnswer !== null){
+    answer = parseInt(userAnswer) === c;
+  } else {
+    answer = userAnswer;
+  }
+
+  return answer;
 }
 
 /**
@@ -52,6 +122,25 @@ function ask() {
  */
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function symbolDecider(number) {
+  let symbol;
+  switch (number) {
+    case 1:
+    symbol = '+';
+    break;
+    case 2:
+    symbol = '-';
+    break;
+    case 3:
+    symbol = '*';
+    break;
+    case 4:
+    symbol = '/';
+    break;
+  }
+  return symbol;
 }
 
 // Byrjar leik
